32d181998ae6431993279b37f9fe5adaf778668e
Arthur Uguen <87493045+artuguen28@users.noreply.github.com>
2022-07-30 23:12:48 -0400

    Classes update

---
+++ jogo_av1/src/pp/Screen.java
@@ -0,0 +1,68 @@
+package pp;
+
+public class Screen{
+		
+	private int vidas;
+	private int enemy_hp;
+	private int x;
+	private int i;
+	private int j;
+	private int turno;
+	private int enemy_x, enemy_y;
+	private int player_x, player_y;
+	
+	Screen(int vidas, int enemy_hp, int x, int turno, int enemy_y, int enemy_x, int player_x, int player_y) {
+		
+		this.vidas = vidas;
+		this.enemy_hp = enemy_hp;
+		this.x = x;
+		this.turno = turno;
+		this.enemy_x = enemy_x;
+		this.enemy_y = enemy_y;
+		this.player_x = enemy_x;
+		this.player_y = enemy_y;
+
+		
+	}
+
+	public void showScreen(){
+		
+		System.out.print("\n\n\n");
+		
+		System.out.println("Player <3: [" + vidas + "] | Enemy HP: [" + enemy_hp + "]");
+		
+		System.out.print("\n");
+		
+		for(i=0; i< 10; i++){
+			for(j=0; j< 10; j++) {
+				
+				if (i == enemy_y && j == enemy_x){
+					System.out.print(".v.");
+					
+				} else if  (i == player_y && j == player_x){
+					System.out.print("{°}");
+					
+				} else {
+					System.out.print(" _|");
+				}
+			}
+			System.out.print("\n");
+			
+		}
+
+		System.out.print("\n");
+		System.out.println("Turno: " + turno);
+		System.out.print("\n");
+		System.out.println("GAME CONTROLS: ");
+		System.out.println(" ___________");
+		System.out.println("|           |");
+		System.out.println("|     ^     |");
+		System.out.println("|     8     |");
+		System.out.println("|< 4     6 >|");
+		System.out.println("|     2     |");
+		System.out.println("|     v     |");
+		System.out.println("|___________|");
+		
+	}
+	
+}
--- jogo_av1/src/pp/main.java
+++ jogo_av1/src/pp/main.java
@@ -1,10 +1,28 @@
-package projeto_teste;
+package pp;
+
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		
+		int x = 1;
+		int vidas = 5;
+		int enemy_hp = 100;
+		int turno = 0;
+		int player_x = 6, player_y = 3;
+		Enemie enem = new Enemie();
+		
+		while(x != 0) {
+			Screen sc = new Screen(vidas, enemy_hp, x, turno, enem.getY_pos(), enem.getX_pos(), player_x, player_y);
+			sc.showScreen();
+			turno ++;
+			enem.enemieMove();
+			Scanner in = new Scanner(System.in);
+			x = in.nextInt();
+		}
+		
+		System.out.print("You left the game!");
 
 	}
-
 }
