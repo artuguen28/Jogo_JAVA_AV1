35e6d919fc9b37883a588f98b800a248b31cbcf0
Arthur Uguen <87493045+artuguen28@users.noreply.github.com>
2022-07-30 23:11:36 -0400

    Class enemie and movements

--- jogo_av1/src/pp/main.java
+++ jogo_av1/src/pp/main.java
@@ -1,11 +1,10 @@
-package pp;
+package projeto_teste;
 
 public class main {
 
 	public static void main(String[] args) {
-		System.out.printf("Hello World!\n");
-		System.out.println("Hello, User!");
-		System.out.println("New line");
+		
+
 	}
 
 }
