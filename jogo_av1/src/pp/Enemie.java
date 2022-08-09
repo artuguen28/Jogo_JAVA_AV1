6a1573446c0bbdc6f6943c89ab9f6e8e2dcbd112
Guilherme Correia Tapajós Araújo <gcta.snf21@uea.edu.br>
2022-08-08 23:41:27 -0400

    Shoot animations

--- jogo_av1/src/pp/Enemie.java
+++ jogo_av1/src/pp/Enemie.java
@@ -6,8 +6,8 @@ public class Enemie {
 	
 	Random random = new Random();
 	private String direct = "";
-	private int x_pos = random.nextInt(10);
-	private int y_pos = random.nextInt(6);
+	private int x_pos = 8;
+	private int y_pos = 8;
 
 	
 	public void enemieMove() {
@@ -17,22 +17,22 @@ public class Enemie {
 		switch (randomInt) {
 			case 0:
 				direct = "Right";
-				if (x_pos >= 9) x_pos = 9;
+				if (x_pos >= 8) x_pos = 8;
 				else x_pos ++;
 				break;
 			case 1:
 				direct = "Up";
-				if (y_pos <= 0) y_pos = 0;
+				if (y_pos <= 1) y_pos = 1;
 				else y_pos --;
 				break;
 			case 2:
 				direct = "Left";
-				if (x_pos <= 0) x_pos = 0;
+				if (x_pos <= 1) x_pos = 1;
 				else x_pos --;
 				break;
 			case 3:
 				direct = "Down";
-				if (y_pos >= 9) y_pos = 9;
+				if (y_pos >= 8) y_pos = 8;
 				else y_pos ++;
 				break;
 			default:
@@ -40,6 +40,33 @@ public class Enemie {
 				break;
 		}
 		System.out.println("Direction: " + direct);
+	}
+
+	public void updateXY() {
+		
+		int randomInt = random.nextInt(4);
+		
+		switch (randomInt) {
+			case 0:
+				this.x_pos = 1;
+				this.y_pos = 1;
+				break;
+				
+			case 1:
+				this.x_pos = 8;
+				this.y_pos = 1;
+				break;
+			case 2:
+				this.x_pos = 8;
+				this.y_pos = 8;
+				break;
+			case 3:
+				this.x_pos = 1;
+				this.y_pos = 8;
+				break;
+			default:
+				break;
+		}
 	}
 
 	public int getX_pos() {
--- jogo_av1/src/pp/Player.java
+++ jogo_av1/src/pp/Player.java
@@ -2,11 +2,14 @@ package pp;
 
 import java.util.Random;
 
+
 public class Player {
 	
+	Random random = new Random();
 	public String direct = "";
-	protected int x_pos;
-	protected int y_pos;
+	private int x_pos;
+	private int y_pos;
+	private int dir = 2;
 
 	public void playerMove(int comand) {
 		
@@ -15,21 +18,25 @@ public class Player {
 				direct = "Right";
 				if (x_pos >= 9) x_pos = 9;
 				else x_pos ++;
+				dir = 6;
 				break;
 			case 8:
 				direct = "Up";
 				if (y_pos <= 0) y_pos = 0;
 				else y_pos --;
+				dir = 8;
 				break;
 			case 4:
 				direct = "Left";
 				if (x_pos <= 0) x_pos = 0;
 				else x_pos --;
+				dir = 4;
 				break;
 			case 2:
 				direct = "Down";
 				if (y_pos >= 9) y_pos = 9;
 				else y_pos ++;
+				dir = 2;
 				break;
 			default:
 				direct = "";
@@ -37,6 +44,290 @@ public class Player {
 		}
 		System.out.println("Direction: " + direct);
 	}
+	
+	public boolean shoot(int x_enemy, int y_enemy, int direct) {
+		
+		switch (direct) {
+			case 6:
+				for(int e=0; e< 10; e++){
+					for(int k=0; k < 10; k++) {
+						if (e == y_pos && k > x_pos && k <= 9){
+							System.out.print(" > ");
+							
+						}
+						else {
+							if (e == (y_enemy + 1) && k == x_enemy){
+								System.out.print("{v}");
+								
+							}else if (e == (y_enemy - 1) && k == x_enemy){
+								System.out.print("{^}");
+								
+							}else if (e == (y_enemy) && k == (x_enemy + 1)){
+									System.out.print(" > ");
+									
+							}else if (e == (y_enemy) && k == (x_enemy - 1)){
+								System.out.print(" < ");
+								
+							}else if (e == y_enemy && k == x_enemy){
+								System.out.print("|O|");
+						}
+							else if (e == y_pos && k == x_pos){
+								System.out.print("{°}");
+								
+							}
+						else
+						{
+							System.out.print(" _|");
+						}
+						
+					}
+				}
+					System.out.print("\n");
+				}
+				System.out.print("\n");
+				if ( x_pos < x_enemy && (y_pos == y_enemy ||  y_pos == (y_enemy + 1) || y_pos == (y_enemy - 1))) {
+					System.out.print("\n");
+					for(int i=0; i< 10; i++){
+						for(int j=0; j< 10; j++) {
+							if (i == y_enemy && j == x_enemy){
+								System.out.print(" X|");
+								
+							} else if  (i == y_pos && j == x_pos){
+								System.out.print("{°}");
+								
+							} else {
+								if (i == y_pos && j > x_pos && j <= 9){
+									System.out.print(" > ");
+								}
+								else {
+								System.out.print(" _|");
+								}
+							}
+						}
+						System.out.print("\n");
+					}	
+				return true;
+					//TimeUnit.SECONDS.sleep(1);
+				} else {
+					return false;
+				}
+		case 8:
+			for(int e=0; e< 10; e++){
+				for(int k=0; k < 10; k++) {
+					if (k == x_pos && e < y_pos && e >= 0){
+						System.out.print(" ^ ");
+
+					}
+					else {
+						if (e == (y_enemy + 1) && k == x_enemy){
+							System.out.print("{v}");
+							
+						}else if (e == (y_enemy - 1) && k == x_enemy){
+							System.out.print("{^}");
+							
+						}else if (e == (y_enemy) && k == (x_enemy + 1)){
+								System.out.print(" > ");
+								
+						}else if (e == (y_enemy) && k == (x_enemy - 1)){
+							System.out.print(" < ");
+							
+						}else if (e == y_enemy && k == x_enemy){
+							System.out.print("|O|");
+					}
+						else if (e == y_pos && k == x_pos){
+							System.out.print("{°}");
+							
+						}
+					else
+					{
+						System.out.print(" _|");
+					}
+					
+				}
+			}
+				System.out.print("\n");
+			}
+			System.out.print("\n");
+				
+				if ( y_pos > y_enemy && (x_pos == x_enemy ||  x_pos == (x_enemy + 1) || x_pos == (x_enemy - 1))) {
+					System.out.print("\n");
+					for(int i=0; i< 10; i++){
+						for(int j=0; j< 10; j++) {
+							
+							if (i == y_enemy && j == x_enemy){
+								System.out.print(" X ");
+								
+							} else if  (i == y_pos && j == x_pos){
+								System.out.print("{°}");
+								
+							}
+							else 
+							{
+								if (j == x_pos && i < y_pos && i >= 0)
+								{
+									System.out.print(" ^ ");
+								}
+								else {
+								System.out.print(" _|");
+								}
+							}
+						}
+						System.out.print("\n");
+					}	
+				return true;
+					//TimeUnit.SECONDS.sleep(1);
+				} else {
+					return false;
+				}
+				
+			case 4:
+				for(int e=0; e< 10; e++){
+					for(int k=0; k < 10; k++) {
+						if (e == y_pos && k < x_pos && k >= 0){
+							System.out.print(" < ");
+						}
+						else {
+							if (e == (y_enemy + 1) && k == x_enemy){
+								System.out.print("{v}");
+								
+							}else if (e == (y_enemy - 1) && k == x_enemy){
+								System.out.print("{^}");
+								
+							}else if (e == (y_enemy) && k == (x_enemy + 1)){
+									System.out.print(" > ");
+									
+							}else if (e == (y_enemy) && k == (x_enemy - 1)){
+								System.out.print(" < ");
+								
+							}else if (e == y_enemy && k == x_enemy){
+								System.out.print("|O|");
+						}
+							else if (e == y_pos && k == x_pos){
+								System.out.print("{°}");
+								
+							}
+						else
+						{
+							System.out.print(" _|");
+						}
+						
+					}
+				}
+					System.out.print("\n");
+				}
+				System.out.print("\n");
+				if ( x_pos > x_enemy && (y_pos == y_enemy ||  y_pos == (y_enemy + 1) || y_pos == (y_enemy - 1))) {
+					System.out.print("\n");
+					for(int i=0; i< 10; i++){
+						for(int j=0; j< 10; j++) {
+							
+							if (i == y_enemy && j == x_enemy){
+								System.out.print(" X|");
+								
+							} else if  (i == y_pos && j == x_pos){
+								System.out.print("{°}");
+								
+							} else {
+								if (i == y_pos && j > x_pos){
+									System.out.print(" < ");
+								}
+								else {
+								System.out.print(" _|");
+								}
+							}
+						}
+						System.out.print("\n");
+					}
+				return true;
+					//TimeUnit.SECONDS.sleep(1);
+				} else {
+					return false;
+				}
+
+				
+			case 2:
+				for(int e=0; e< 10; e++){
+					for(int k=0; k < 10; k++) {
+						if (k == x_pos && e > y_pos && e <= 9){
+							System.out.print(" v ");
+
+						}
+						else {
+							if (e == (y_enemy + 1) && k == x_enemy){
+								System.out.print("{v}");
+								
+							}else if (e == (y_enemy - 1) && k == x_enemy){
+								System.out.print("{^}");
+								
+							}else if (e == (y_enemy) && k == (x_enemy + 1)){
+									System.out.print(" > ");
+									
+							}else if (e == (y_enemy) && k == (x_enemy - 1)){
+								System.out.print(" < ");
+								
+							}else if (e == y_enemy && k == x_enemy){
+								System.out.print("|O|");
+						}
+							else if (e == y_pos && k == x_pos){
+								System.out.print("{°}");
+								
+							}
+						else
+						{
+							System.out.print(" _|");
+						}
+						
+					}
+				}
+					System.out.print("\n");
+				}
+				System.out.print("\n");
+				
+				if ( y_pos < y_enemy && (x_pos == x_enemy ||  x_pos == (x_enemy + 1) || x_pos == (x_enemy - 1))) {
+					System.out.print("\n");
+					for(int i=0; i< 10; i++){
+						for(int j=0; j< 10; j++) {
+							
+							if (i == y_enemy && j == x_enemy){
+								System.out.print(" X ");
+								
+							} else if  (i == y_pos && j == x_pos){
+								System.out.print("{°}");
+								
+							} else {
+								if (j == x_pos && i > y_pos && i <= 9)
+								{
+									System.out.print(" v ");
+								}
+								else {
+								System.out.print(" _|");
+								}
+							}
+						}
+						System.out.print("\n");
+					}
+				return true;
+					//TimeUnit.SECONDS.sleep(1);
+				} else {
+					return false;
+				}
+			
+			default:
+				return false;
+		}
+	}
+	public boolean colision(int x_enemy, int y_enemy) {
+		if((x_pos == (x_enemy + 1) &&  y_pos == (y_enemy + 1)) || (x_pos == (x_enemy - 1) && (y_pos == (y_enemy - 1)))) {
+			return true;
+		}
+		else return false;
+	}
+	
+	public void updateXY() {
+		this.x_pos = random.nextInt(10);
+		this.y_pos = random.nextInt(10);
+	}
+
 
 	public int getX_pos() {
 		return x_pos;
@@ -46,6 +337,7 @@ public class Player {
 	public int getY_pos() {
 		return y_pos;
 	}
+	
 
 
 }
